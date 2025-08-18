c26ac4d7ee66d9912d5a41696179b1190a0353f1
Paul Girard <paul@ouestware.com>
2025-08-18 19:27:39 +0200

    langswitcher based on filename

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  i18n: {
+    // to add a locales just translate one page by using `{countrycode2}.astro` as filename
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 });
