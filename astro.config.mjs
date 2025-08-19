6e3a3ba4aa518ad713741cd45744889bc1817b2b
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-08-19 16:54:55 +0200

    Adds various icons to the landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  integrations: [icon()],
   i18n: {
     // to add a locales just translate one page by using `{countrycode2}.astro` as filename
     locales: ["en"],
