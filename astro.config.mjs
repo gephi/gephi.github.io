b98ec2ac6c6671e75982ad0412957ae22ef26fb7
Paul Girard <paul@ouestware.com>
2025-10-08 14:00:45 +0200

    [site] added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import redirects from "./redirects";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://gephi.org",
   output: "static",
-  integrations: [icon(), react()],
+  integrations: [icon(), react(), sitemap()],
   i18n: {
     // to add a locales just translate one page by using `{countrycode2}.astro` as filename
     locales: ["en"],
